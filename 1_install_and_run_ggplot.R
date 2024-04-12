aedd2cd18ce43a5ca5241ba455a3c8eba16f558a
Leo-Beh <zi.beh2@mail.dcu.ie>
2024-04-12 13:56:14 +0100

    q1 upload

---
+++ 1_install_and_run_ggplot.R
@@ -0,0 +1,11 @@
+#installing the ggplot2 library...
+# install.packages("ggplot2")
+
+#Load the ggplot2 library
+library("ggplot2")
+
+#read and load the "companies" dataset into R using a downlaoded clean version
+companies_df <- read_excel("companies.xlsx")
+
+#testing that ggplot is installed and loaded...
+ggplot(data = companies_df, aes(x = Ratings, y = Total_reviews))
